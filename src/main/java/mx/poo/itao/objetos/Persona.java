880127d1ca587febf572327b27f3d889abe3a16c
gaely <gaely@Arath>
2025-04-29 08:27:54 -0600

    .

--- src/main/java/mx/poo/itao/objetos/Persona.java
+++ src/main/java/mx/poo/itao/objetos/Persona.java
@@ -14,9 +14,20 @@ import lombok.experimental.SuperBuilder;
 public class Persona {
 	
 	private String nombre;
+	private String apellido;
+	private int edad;
+	private double estatura;
 	
 	public void saludar() {
-		System.out.println("Hola, soy " + nombre);
+		System.out.println("Hola, soy " + nombre + apellido);
 	}
+		public void edad() {
+			System.out.println("Tengo" + edad);
+		}
+			public void Medida() {
+				System.out.println("Mido" + estatura);
+			
+	}
+		
 
 }
