71b6fa348a632d1be540f3f552e1c6a0411d2ad6
Yuli 777 <ing.fergam@gmail.com>
2025-05-01 20:16:23 -0600

    memo huevon

--- src/main/java/mx/poo/itao/objetos/Profesor.java
+++ src/main/java/mx/poo/itao/objetos/Profesor.java
@@ -5,6 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Profesor extends Persona {
 	private String numEm;
